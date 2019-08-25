efb5b6f5da3ca56f8cbffbcb24a1656e8ac2e692
Dmitrii Iurin <dimon07710@yandex.ru>
2019-08-25 22:54:36 +0300

    Install device gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,23 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
   has_many :events
+
+  before_validation :set_name, on: :create
+
   # Имя не не более 35 символов
   validates :name, presence: true, length: { maximum: 35 }
-  # Уникальный email по заданному шаблону не более 255
-  # символов
-  validates :email, presence: true, length: { maximum: 255 }
+
   validates :email, uniqueness: true
-  validates :email, format: { with: /\A([^@\s]+)@(([-[:word:]]+\.)+[[:word:]]{2,})\z/,
-                              message: 'is not an email' }
+
+  private
+
+  def set_name
+    self.name = "Товарисч №#{rand(777)}" if name.blank?
+  end
 end
