fb52d2461df1f43837663202a14459bcadb7c153
Dmitrii Iurin <dimon07710@yandex.ru>
2019-09-23 23:21:32 +0300

    add Comment model

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,7 @@
 
 class Event < ApplicationRecord
   belongs_to :user
+  has_many :comments, dependent: :destroy
 
   validates :title, presence: true, length: { maximum: 255 }
   validates :address, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :trackable
 
   has_many :events
+  has_many :comments, dependent: :destroy
 
   before_validation :set_name, on: :create
 
