c3b6068a5c96bd9088571bd7dec382e21030dc85
Dmitrii Iurin <dimon07710@yandex.ru>
2019-10-02 23:37:12 +0300

    Added pincode access to the event

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_29_163953) do
+ActiveRecord::Schema.define(version: 2019_10_02_193012) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2019_09_29_163953) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "pincode"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
