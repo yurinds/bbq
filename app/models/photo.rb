b26881bc6f80974e3fc47acd882f9b73c9603e3b
Dmitrii Iurin <dimon07710@yandex.ru>
2019-10-06 14:34:16 +0300

    delete scope

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -4,9 +4,6 @@ class Photo < ApplicationRecord
   belongs_to :event
   belongs_to :user
 
-  scope :first_six, -> { order(:created_at).limit(6) }
-  scope :seven_and_all, -> { order(:created_at).offset(6) }
-
   validates :photo, presence: true
 
   mount_uploader :photo, PhotoUploader
